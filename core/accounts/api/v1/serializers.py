dfb70595cfd8c115a7fa66b333e682712a67e7ed
alpha330 <43549306+alpha330@users.noreply.github.com>
2023-11-17 20:13:40 +0330

    update2
    complete registration api

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
 from accounts.models import Users
-
+from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions
 class RegistrationSerializer(serializers.ModelSerializer):
+    password_1= serializers.CharField(max_length=255,write_only=True)
     class Meta:
         model = Users
-        fields = ["email","password"]
+        fields = ["email","password","password_1"]
     def validate(self,attrs):
+        if attrs.get("password") != attrs.get("password_1"):
+            raise serializers.ValidationError({"detail":"Password dose not match"})
+        try:
+            validate_password(attrs["password"])
+        except exceptions.ValidationError as e:
+            raise serializers.ValidationError({"password":e.messages})
+            
         return super().validate(attrs)
     def create(self,validated_data):
-        return super().create(validated_data)
+        validated_data.pop("password_1")
+        return Users.objects.create_user(**validated_data)
--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegistrationSerializer
+
 class RegistrationApiView(generics.GenericAPIView):
     serializer_class = RegistrationSerializer
     
@@ -13,3 +14,4 @@ class RegistrationApiView(generics.GenericAPIView):
                 "email":serializer.validated_data["email"]
             }
             return Response(data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
